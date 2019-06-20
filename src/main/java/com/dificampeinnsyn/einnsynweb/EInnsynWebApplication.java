c874fb492914a17861ba007fd410b9dd88087747
oisteinhaugland <haugland.1193@gmail.com>
2019-06-20 09:00:46 +0200

    Add default java program to github repository

---
+++ src/main/java/com/dificampeinnsyn/einnsynweb/EInnsynWebApplication.java
@@ -0,0 +1,13 @@
+package com.dificampeinnsyn.einnsynweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInnsynWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EInnsynWebApplication.class, args);
+    }
+
+}
