343d7eacb0c5c6bc2bd932327cee5eb022a265eb
oisteinhaugland <haugland.1193@gmail.com>
2019-06-20 12:25:04 +0200

    Add azure db connection

--- src/main/java/com/dificampeinnsyn/einnsynweb/EInnsynWebApplication.java
+++ src/main/java/com/dificampeinnsyn/einnsynweb/EInnsynWebApplication.java
@@ -1,25 +1,69 @@
 package com.dificampeinnsyn.einnsynweb;
 
-import com.dificampeinnsyn.einnsynweb.readFile.readLines;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 @SpringBootApplication
 public class EInnsynWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EInnsynWebApplication.class, args);
-        File f = new File("TEXT_FILES/minidata.txt");
+
+
+        //SpringApplication.run(EInnsynWebApplication.class, args);
+
+        // Connect to database
+        String hostName = "mysqlserver794613852.database.windows.net"; // update me
+        String dbName = "sampleDb"; // update me
+        String user = "azureuser"; // update me
+        String password = "@dificamp2019"; // update me
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            System.out.println("Query data example:");
+            System.out.println("=========================================");
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT * " + "FROM [dbo].[Persons] ";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+                // Print results from select statement
+                System.out.println("Top 20 categories:");
+                while (resultSet.next())
+                {
+                    System.out.println(resultSet.getString(1) + " "
+                            + resultSet.getString(2));
+                }
+                connection.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+        /*  File f = new File("TEXT_FILES/minidata.txt");
         try {
             readLines.readLine(f);
         } catch (IOException e){
             e.printStackTrace();
         }
-
+*/
     }
 
 }
