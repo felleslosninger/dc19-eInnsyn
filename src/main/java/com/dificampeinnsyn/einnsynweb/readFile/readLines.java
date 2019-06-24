0f353549e48b724742e18de7810ab3fc7cadef7a
oisteinhaugland <haugland.1193@gmail.com>
2019-06-24 12:57:28 +0200

    Update readTurtle function

--- src/main/java/com/dificampeinnsyn/einnsynweb/readFile/readLines.java
+++ src/main/java/com/dificampeinnsyn/einnsynweb/readFile/readLines.java
@@ -35,12 +35,51 @@ public class readLines {
         while((line = br.readLine()) != null){
 
             if (line.matches(pattern)){ //finds all journalposts
-                System.out.println(line);
-                for (int i = 0;i<13;i++){
-                    String attrLine = br.readLine();
-                    System.out.println(attrLine);
+                String attrLine;
+                String location;
+                String temp[];
+
+                temp = line.split(" ");
+                location = temp[0].substring(1,temp[0].length()-1);
+
+                String attrPattern = "(.*> \\.)|(.*> ;)|(.* ;)"; //correct atm.
+                String attrSplitPattern = "arkiv:.*? ";
+                String attrSplitPatternSpecial = " ";
+                String specialCase = "arkiv:journalposttype.*";
+                String numberPattern = "^\\d.+";
+                //sql insert statement here
+                for (int i = 0;i<13;i++){ //attributes
+                    if((attrLine = br.readLine()).matches(attrPattern) && attrLine != "") {
+                        if(attrLine.contains("arkiv:journalposttype")){
+                            String jPTypeTemp[] = attrLine.split(" ");
+                            String jPType = jPTypeTemp[1];
+
+                            System.out.println(jPType);
+                        } else {
+                                String superTemp[] = attrLine.split(attrSplitPattern);
+                            try {
+                                String valueTemp = superTemp[1];
+                                String StringValue = valueTemp.substring(0,valueTemp.length()-2);
+
+                                if (StringValue.matches(numberPattern)){
+                                    int value = Integer.parseInt(StringValue);
+                                    System.out.println(value);
+
+                                }  else {
+                                    String value = StringValue;
+                                    System.out.println(value);
+                                }
+
+                            } catch(Exception e){
+
+                            }
+
+                        }
+
+                    //sql insert statement here
+                    }
                 }
-                System.out.println("");
+                System.out.println("END OF OBJECT");
                 System.out.println("");
             }
 
