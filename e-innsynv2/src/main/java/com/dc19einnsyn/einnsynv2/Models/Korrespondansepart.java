8e86cc41f9f10499996cddec51bb5d8958d7d263
Cathinka Johnsen <cathygutto@gmail.com>
2019-06-25 12:12:50 +0200

    add registrering

--- e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Korrespondansepart.java
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Korrespondansepart.java
@@ -1,11 +1,12 @@
 package com.dc19einnsyn.einnsynv2.Models;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 @Entity
 @Data
-
+@NoArgsConstructor
 public class Korrespondansepart {
 
     private String korrespondanseparttype;
--- e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Registrering.java
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Registrering.java
@@ -1,4 +1,39 @@
 package com.dc19einnsyn.einnsynv2.Models;
 
+import java.util.Set;
+
 public class Registrering {
+    private String systemID;
+    private String opprettetDato;
+    private String opprettetAv;
+    private String arkivertDato;
+    private String arkivertAv;
+    private String referanseArkivdel;
+
+    private String kassasjon;
+    private String skjerming;
+    private String gradering;
+
+    private String dokumentbeskrivelse;
+    private String dokumentobjekt;
+
+    private String korrespondansepart;
+
+    private String registreringsID;
+    private String tittel;
+    private String offentligTittel;
+    private String beskrivelse;
+    private String noekkelord;
+    private String forfatter;
+    private String dokumentmedium;
+    private String oppbevaringssted;
+    private String virksomhetsspesifikkeMetadata;
+
+    private String merknad;
+    private String kryssreferanse;
+
+    
+    Set<Korrespondansepart> korrespondanseparter;
+    Set<Dokumentbeskrivelse> dokumentbeskrivelser;
+    Set<Skjerming> skjerminger;
 }
