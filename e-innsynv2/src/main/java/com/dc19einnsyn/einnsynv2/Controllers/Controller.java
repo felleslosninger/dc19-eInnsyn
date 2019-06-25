1a9aea187997944be8d9993343f6adc451203ba6
oisteinhaugland <haugland.1193@gmail.com>
2019-06-25 10:31:49 +0200

    Create new project and creat folder structure

---
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Controllers/Controller.java
@@ -0,0 +1,4 @@
+package com.dc19einnsyn.einnsynv2.Controllers;
+
+public class Controller {
+}
---
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/EInnsynv2Application.java
@@ -0,0 +1,13 @@
+package com.dc19einnsyn.einnsynv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInnsynv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EInnsynv2Application.class, args);
+    }
+
+}
