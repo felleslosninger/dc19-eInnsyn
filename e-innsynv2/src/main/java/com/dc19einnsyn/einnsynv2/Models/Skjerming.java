3e340031fccb997219b85fe3eac37915cad47997
Thomas Yttri <thoytt04@hotmail.com>
2019-06-25 11:04:26 +0200

    To create Dokbes and Skjerming classes.

--- e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Skjerming.java
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Skjerming.java
@@ -1,4 +1,27 @@
 package com.dc19einnsyn.einnsynv2.Models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Skjerming {
+    private String tilgangsrestriksjon;
+    private String skjermingshjemmel;
+    private String skjermingMetadata;
+    private String skjermingDokument;
+    private String skjjermingsvarighet;
+    private String skjermingOpphoererData;
+
+    /*
+    <xs:element name="tilgangsrestriksjon" type="n5mdk:tilgangsrestriksjon"/>
+    <xs:element name="skjermingshjemmel" type="n5mdk:skjermingshjemmel"/>
+    <xs:element name="skjermingMetadata" type="n5mdk:skjermingMetadata" maxOccurs="unbounded"/>
+    <xs:element name="skjermingDokument" type="n5mdk:skjermingDokument" minOccurs="0"/>
+    <xs:element name="skjermingsvarighet" type="n5mdk:skjermingsvarighet" minOccurs="0"/>
+    <xs:element name="skjermingOpphoererDato" type="n5mdk:skjermingOpphoererDato" minOccurs="0"/>
+     */
 }
