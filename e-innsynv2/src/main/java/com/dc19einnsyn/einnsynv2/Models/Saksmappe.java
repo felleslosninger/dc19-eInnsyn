dd0c116b4d3add0f03b4fb67a18b6b8aed7ac1f7
Randi Gustad <randi.gustad@difi.no>
2019-06-25 12:03:49 +0200

    Add attributes in Saksmappe

--- e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Saksmappe.java
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/Models/Saksmappe.java
@@ -1,4 +1,34 @@
 package com.dc19einnsyn.einnsynv2.Models;
 
-public class Saksmappe {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "tbl_person")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Saksmappe extends Mappe{
+    private String saksaar;
+    private String sakssekvensnummer;
+    private String saksdato;
+    private String adminstrativEnhet;
+    private String saksansvarlig;
+    private String journalenhet;
+    private String saksstatus;
+    private String utlaantDato;
+    private String utlaantTil;
+    private String regeranseSekundaerKassifikasjon;
+    private String sakspart;
+    private String presidens;
+
+
+
+
 }
