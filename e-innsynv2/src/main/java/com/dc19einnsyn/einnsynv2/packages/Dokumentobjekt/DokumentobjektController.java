21f2427bd37dbbcaca7ad2a566e240d325dfecac
oisteinhaugland <haugland.1193@gmail.com>
2019-07-02 09:29:40 +0200

    Add @RestController annotation to DokumentobjektController

--- e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/packages/Dokumentobjekt/DokumentobjektController.java
+++ e-innsynv2/src/main/java/com/dc19einnsyn/einnsynv2/packages/Dokumentobjekt/DokumentobjektController.java
@@ -1,4 +1,10 @@
 package com.dc19einnsyn.einnsynv2.packages.Dokumentobjekt;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DokumentobjektController {
+
+
+
 }
