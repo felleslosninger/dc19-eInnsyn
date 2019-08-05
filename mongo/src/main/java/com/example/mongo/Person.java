7177de3cff3136dd257b0d4a3defbe3678df9f4b
cathygutto <32327765+cathygutto@users.noreply.github.com>
2019-08-05 09:52:17 +0200

    MongoDB-SpringBoot

---
+++ mongo/src/main/java/com/example/mongo/Person.java
@@ -0,0 +1,34 @@
+package com.example.mongo;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "personer")
+public class Person {
+
+    @Id private String id;
+
+    @Field(value="FirstName")
+    private String firstName;
+
+    @Field(value="LastName")
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
